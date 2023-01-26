f6c83e643882b636ca5fc4d198261fb4806207f3
semantic-release <semantic-release>
2023-01-26 17:02:20 +0100

    0.5.0
    Automatically generated by python-semantic-release

--- check_pa/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Ralph Offinger'
-__email__ = 'ralph.offinger@gmail.com'
-__version__ = '0.3.2'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 
 setup(name='check_paloalto_ng',
       version=__version__,
