c51d12ef6ecaa3e046f5ded93c3bb78cb79c19b7
Christian Jonak <christian@jonak.org>
2023-08-23 14:53:06 +0200

    fix: disk check on nvme based firewalls fixes #1

--- check_pa/modules/diskspace.py
+++ check_pa/modules/diskspace.py
@@ -40,12 +40,11 @@ class DiskSpace(np.Resource):
         """
         _log.info('Reading XML from: %s', self.xml_obj.build_request_url())
         soup = self.xml_obj.read()
-        available_disks = re.findall('((sda\d.*?)|(md\d.*?)|(mmcblk\d*p\d.*?))(?=/)', soup.result.string)
-#        available_disks = re.findall('(mmcblk\d*p\d.*?)(?=/)', soup.result.string)
+        available_disks = re.findall('((sda\d.?)|(md\d.?)|(mmcblk\dp\d.?)|(nvme\dn\dp\d.*?))(?=/)', soup.result.string)
 
         for disk in available_disks:
             _log.debug("disk: "+str(disk))
-            diskname = re.findall('((sda\d)|(mmcblk\d*p\d)|(md\d))', disk[0])[0][0]
+            diskname = re.findall('((sda\d)|(mmcblk\dp\d)|(md\d)|(nvme\dn\dp\d))', disk[0])[0][0]
             diskname = str(diskname)
             _log.debug("name: "+str(diskname))
             percent = int(re.findall('([0-9]+%)', disk[0])[0].replace("%", ""))
